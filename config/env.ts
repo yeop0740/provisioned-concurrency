789ca8cdf3ae25e24c762d690af01c2c7ef2cbaf
heeyeop <ssucgoo@gmail.com>
2025-03-04 09:39:18 +0900

    :closed_lock_with_key: 환경 변수 설정

---
+++ config/env.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+import {convertToPascalCase, convertToUpperCaseAll} from "../util";
+
+dotenv.config();
+
+export const ACCOUNT_ID = process.env.ACCOUNT_ID ?? (() => { throw new Error('환경 변수 ACCOUNT_ID 이(가) 설정되지 않았습니다.'); })();
+export const REGION = process.env.REGION ?? (() => { throw new Error('환경 변수 REGION 이(가) 설정되지 않았습니다.'); })();
+
+export const GRAPHQL_LAMBDA_NAME = process.env.GRAPHQL_LAMBDA_NAME ?? (() => { throw new Error('환경 변수 GRAPHQL_LAMBDA_NAME 이(가) 설정되지 않았습니다.'); })();
+
+export const NODE_ENV = process.env.NODE_ENV ?? 'dev';
+export const NODE_ENV_PASCAL = convertToPascalCase(NODE_ENV);
+export const NODE_ENV_UPPER = convertToUpperCaseAll(NODE_ENV);
+
+export const DINNER_TIME_START = process.env.DINNER_TIME_START ?? (() => { throw new Error('환경 변수 DINNER_TIME_START 이(가) 설정되지 않았습니다.'); })();
+export const DINNER_TIME_END = process.env.DINNER_TIME_END ?? (() => { throw new Error('환경 변수 DINNER_TIME_END 이(가) 설정되지 않았습니다.'); })();
