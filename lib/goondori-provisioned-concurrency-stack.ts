4302d080633e3ddd159a529dd13c2dda2ecbbd70
heeyeop <ssucgoo@gmail.com>
2025-03-04 17:21:57 +0900

    :sparkles: lambda version 에 대해 alias 생성, 해당 alias 를 invoke 하는 api gateway 생성 및 통합

--- lib/goondori-provisioned-concurrency-stack.ts
+++ lib/goondori-provisioned-concurrency-stack.ts
@@ -1,11 +1,23 @@
 import * as cdk from "aws-cdk-lib";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as applicationautoscaling from "aws-cdk-lib/aws-applicationautoscaling";
+import { Alias, Function, Version } from "aws-cdk-lib/aws-lambda";
+import {
+  ScalableTarget,
+  Schedule,
+  ServiceNamespace,
+} from "aws-cdk-lib/aws-applicationautoscaling";
+import {
+  CorsHttpMethod,
+  HttpApi,
+  HttpMethod,
+} from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import { Construct } from "constructs";
 import {
   DINNER_TIME_END,
   DINNER_TIME_START,
   GRAPHQL_LAMBDA_NAME,
+  GRAPHQL_LAMBDA_VERSION,
+  NODE_ENV,
   NODE_ENV_PASCAL,
   NODE_ENV_UPPER,
   TEST_TIME_END,
@@ -17,22 +29,59 @@ export class GoondoriProvisionedConcurrencyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const graphqlLambda = lambda.Function.fromFunctionName(
+    const graphqlLambda = Function.fromFunctionName(
       this,
       `GraphqlLambda${NODE_ENV_PASCAL}`,
       GRAPHQL_LAMBDA_NAME
     );
-
-    const lambdaAlias = new lambda.Alias(this, `Alias${NODE_ENV_PASCAL}Dev`, {
+    const graphqlLambdaVersion = Version.fromVersionAttributes(
+      this,
+      `GoondoriGraphqlLambdaVersion${NODE_ENV_PASCAL}`,
+      {
+        lambda: graphqlLambda,
+        version: GRAPHQL_LAMBDA_VERSION,
+      }
+    );
+    const lambdaAlias = new Alias(this, `Alias${NODE_ENV_PASCAL}Dev`, {
       aliasName: NODE_ENV_UPPER,
-      version: graphqlLambda.latestVersion,
+      version: graphqlLambdaVersion,
     });
 
-    const scalingTarget = new applicationautoscaling.ScalableTarget(
+    const apiGateway = new HttpApi(this, `ApiGateway${NODE_ENV_PASCAL}`, {
+      apiName: `${NODE_ENV}2-goondori-backend`,
+      corsPreflight: {
+        allowHeaders: [
+          "content-type",
+          "x-amz-date",
+          "authorization",
+          "x-api-key",
+          "x-amz-security-token",
+          "x-amz-user-agent",
+          "x-amz-trace-id",
+        ],
+        allowMethods: [
+          CorsHttpMethod.GET,
+          CorsHttpMethod.POST,
+          CorsHttpMethod.OPTIONS,
+        ],
+        allowOrigins: ["*"],
+      },
+    });
+    const integration = new HttpLambdaIntegration(
+      `ApiGatewayIntegration${NODE_ENV_PASCAL}`,
+      lambdaAlias
+    );
+    apiGateway.addRoutes({
+      path: "/",
+      methods: [HttpMethod.GET, HttpMethod.POST],
+      integration: integration,
+    });
+
+    const scalingTarget = new ScalableTarget(
       this,
       `ScalingTarget${NODE_ENV_PASCAL}`,
       {
-        serviceNamespace: applicationautoscaling.ServiceNamespace.LAMBDA,
+        serviceNamespace: ServiceNamespace.LAMBDA,
         maxCapacity: 10,
         minCapacity: 1,
         resourceId: `function:${graphqlLambda.functionName}:${lambdaAlias.aliasName}`,
@@ -44,7 +93,7 @@ export class GoondoriProvisionedConcurrencyStack extends cdk.Stack {
     const dinnerEndTime = convertKSTToUTC(new Date(DINNER_TIME_END));
 
     scalingTarget.scaleOnSchedule(`DinnerTimeScaleOut${NODE_ENV_PASCAL}`, {
-      schedule: applicationautoscaling.Schedule.cron({
+      schedule: Schedule.cron({
         minute: dinnerStartTime.getUTCMinutes().toString(),
         hour: dinnerStartTime.getUTCHours().toString(),
       }),
@@ -53,7 +102,7 @@ export class GoondoriProvisionedConcurrencyStack extends cdk.Stack {
     });
 
     scalingTarget.scaleOnSchedule(`DinnerTimeScaleIn${NODE_ENV_PASCAL}`, {
-      schedule: applicationautoscaling.Schedule.cron({
+      schedule: Schedule.cron({
         minute: dinnerEndTime.getUTCMinutes().toString(),
         hour: dinnerEndTime.getUTCHours().toString(),
       }),
@@ -65,7 +114,7 @@ export class GoondoriProvisionedConcurrencyStack extends cdk.Stack {
     const testEndTime = convertKSTToUTC(new Date(TEST_TIME_END));
 
     scalingTarget.scaleOnSchedule(`TestTimeScaleOut${NODE_ENV_PASCAL}`, {
-      schedule: applicationautoscaling.Schedule.cron({
+      schedule: Schedule.cron({
         minute: testStartTime.getUTCMinutes().toString(),
         hour: testStartTime.getUTCHours().toString(),
       }),
@@ -74,7 +123,7 @@ export class GoondoriProvisionedConcurrencyStack extends cdk.Stack {
     });
 
     scalingTarget.scaleOnSchedule(`TestTimeScaleIn${NODE_ENV_PASCAL}`, {
-      schedule: applicationautoscaling.Schedule.cron({
+      schedule: Schedule.cron({
         minute: testEndTime.getUTCMinutes().toString(),
         hour: testEndTime.getUTCHours().toString(),
       }),
